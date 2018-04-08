fd8592e3436065b3529da7ba82bf0f937c9b4f37
Josh Kassman <jkassman2@gmail.com>
2018-04-07 23:47:02 -0400

    created base file

---
+++ dashapp.py
@@ -0,0 +1,11 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+app.layout = html.Div(children=[
+    html.H1(children='Corec Graph'),
+  ])
+
+if __name__ == "__main__":
+  app.run_server(debug=True)
