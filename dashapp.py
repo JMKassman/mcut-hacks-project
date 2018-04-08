b4d5afe61f77ad40ab5a841a96df02edc6cdadad
Christopher Rosenblatt <rosench13@gmail.com>
2018-04-08 00:07:43 -0400

    add days menu

--- dashapp.py
+++ dashapp.py
@@ -5,6 +5,19 @@ import dash_html_components as html
 app = dash.Dash()
 app.layout = html.Div(children=[
     html.H1(children='Corec Graph'),
+    dcc.Dropdown (
+    	options = [
+    		{'label': 'Sunday', 'value': 0},
+    		{'label': 'Monday', 'value': 1},
+    		{'label': 'Tuesday', 'value': 2},
+    		{'label': 'Wednesday', 'value': 3},
+    		{'label': 'Thursday', 'value': 4},
+    		{'label': 'Friday', 'value': 5},
+    		{'label': 'Saturday', 'value': 6}
+    	],
+    	multi = True,
+    	value = 0
+    )
   ])
 
 if __name__ == "__main__":
